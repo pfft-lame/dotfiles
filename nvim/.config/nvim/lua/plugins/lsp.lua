133f12c00287358239c04a7bbfbc02f9668f010e
Sharath <sharathkota070@gmail.com>
2025-11-08 03:02:03 +0530

    removing redundant clear_auto_command

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -166,14 +166,6 @@ return {
 					-- end)
 					vim.lsp.inlay_hint.enable(true, { bufnr = event.buf })
 				end
-
-				vim.api.nvim_create_autocmd("LspDetach", {
-					group = vim.api.nvim_create_augroup("custom-lsp-detach", { clear = true }),
-					callback = function(event2)
-						vim.lsp.buf.clear_references()
-						vim.api.nvim_clear_autocmds({ group = "kickstart-lsp-highlight", buffer = event2.buf })
-					end,
-				})
 			end,
 		})
 
