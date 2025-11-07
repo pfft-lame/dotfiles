5d2dc147cdac6ea2d33818ee1dd2e76cec44c48a
Sharath <sharathkota070@gmail.com>
2025-11-08 02:57:15 +0530

    changes in tmux, neovim and kanata

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -117,35 +117,62 @@ return {
 					vim.diagnostic.jump({ count = 1 })
 				end, opts)
 
+				local function client_supports_method(client, method, bufnr)
+					if vim.fn.has("nvim-0.11") == 1 then
+						return client:supports_method(method, bufnr)
+					else
+						return client.supports_method(method, { bufnr = bufnr })
+					end
+				end
+
+				local client = vim.lsp.get_client_by_id(event.data.client_id)
+				if
+					client
+					and client_supports_method(
+						client,
+						vim.lsp.protocol.Methods.textDocument_documentHighlight,
+						event.buf
+					)
+				then
+					local highlight_augroup = vim.api.nvim_create_augroup("kickstart-lsp-highlight", { clear = false })
+					vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+						buffer = event.buf,
+						group = highlight_augroup,
+						callback = vim.lsp.buf.document_highlight,
+					})
+
+					vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+						buffer = event.buf,
+						group = highlight_augroup,
+						callback = vim.lsp.buf.clear_references,
+					})
+
+					vim.api.nvim_create_autocmd("LspDetach", {
+						group = vim.api.nvim_create_augroup("kickstart-lsp-detach", { clear = true }),
+						callback = function(event2)
+							vim.lsp.buf.clear_references()
+							vim.api.nvim_clear_autocmds({ group = "kickstart-lsp-highlight", buffer = event2.buf })
+						end,
+					})
+				end
+
+				if
+					client
+					and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf)
+				then
+					-- map("n", "<leader>th", function()
+					-- 	vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
+					-- 	print(vim.lsp.inlay_hint.is_enabled())
+					-- end)
+					vim.lsp.inlay_hint.enable(true, { bufnr = event.buf })
+				end
+
 				vim.api.nvim_create_autocmd("LspDetach", {
 					group = vim.api.nvim_create_augroup("custom-lsp-detach", { clear = true }),
 					callback = function(event2)
 						vim.lsp.buf.clear_references()
-						vim.api.nvim_clear_autocmds({ group = "custom-lsp-highlight", buffer = event2.buf })
+						vim.api.nvim_clear_autocmds({ group = "kickstart-lsp-highlight", buffer = event2.buf })
 					end,
-				})
-
-				-- don't support json filetypes (we don't have lsp)
-				local filetype = vim.bo[bufnr].filetype
-				local fts = { "json", "jsonc", "sh", "yaml", "sql", "eslint" }
-				for _, ft in ipairs(fts) do
-					if ft == filetype then
-						return
-					end
-				end
-
-				-- highlight the references
-				local highlight_augroup = vim.api.nvim_create_augroup("custom-lsp-highlight", { clear = false })
-				vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-					buffer = event.buf,
-					group = highlight_augroup,
-					callback = vim.lsp.buf.document_highlight,
-				})
-
-				vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-					buffer = event.buf,
-					group = highlight_augroup,
-					callback = vim.lsp.buf.clear_references,
 				})
 			end,
 		})
